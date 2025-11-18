61404163783976b374042e61c0cb6e6b8fbdb2aa
Nicolás <nicolmaderamc123@gmail.com>
2025-11-18 13:25:54 -0500

    avance

--- 03-testimonials-grid-section/src/App.jsx
+++ 03-testimonials-grid-section/src/App.jsx
@@ -4,13 +4,27 @@ import { Card } from "./components/Card"
 export const App = () => {
   return(
     <>
+    <div className="container">
     <Card
      cardImage="./public/image-daniel.jpg"
-     cardName="Daniel Cliford"
-     cardDesc="Verified Graduate"
-     cardTitle="I received a job offer mid-course, and the subjects I learned were current, if not more so, in the company I joined. I honestly feel I got every penny's worth."
-     cardDescription="I was an EMT for many years before I joined the bootcamp. I've been looking to make a transition and have heard some people who had an amazing experience here. I signed up for the free intro course and found it incredibly fun! I enrolled shortly thereafter. The next 12 weeks was the best - and most grueling - time of my life. Since completing the course, I've successfully switched careers, working as a Software Engineer at a VR startup."
+
     />
+
+    </div>
+    <div className="container">
+    <Card
+     cardImage="./public/image-daniel.jpg"
+
+    />
+
+    </div>
+    <div className="container">
+    <Card
+     cardImage="./public/image-daniel.jpg"
+
+    />
+
+    </div>
     </>
   )
 }
--- 03-testimonials-grid-section/src/components/Card.jsx
+++ 03-testimonials-grid-section/src/components/Card.jsx
@@ -1,16 +1,20 @@
+import "../stylesheets/Card.css"
 
-
-export const Card = () => {
+export const Card = ({cardImage}) => {
     return (
-        <div className="Card">
-            <div className="m-auto mt-3" style={{width:"80px", height:  "80px"}}>
-                <img className="icon" src={cardImage} alt="" />
-                <h1 className="Name">{cardName}</h1>
-                <p className="NameDescription">{cardDesc}</p>
+        <div className=".Card bg-purple m-auto mt-3 rounded row">
+            <div className=".card-body  d-flex p-2 mt-2 col" >
+                <div>
+                 <img className="icon" src={cardImage} alt="" />
+                </div>
+                <div>
+                 <h1 className=".card-title ps-3 fs-4 ">Daniel Clifford</h1>
+                 <p className=".card-subtitle ps-3">Verified Graduate</p>
+                </div>
             </div>
             <div>
-                <h1 className="Title">{cardTitle}</h1>
-                <p>{cardDescription}</p>
+                <h1 className=".card-title fs-6 mt-1 ms-3">I received a job offer mid-course, and the subjects I learned were current, if not more so, in the company I joined. I honestly feel I got every penny's worth.</h1>
+                <p className=".card-subtitle fs7 mt-2 ms-3">I was an EMT for many years before I joined the bootcamp. I've been looking to make a transition and have heard some people who had an amazing experience here. I signed up for the free intro course and found it incredibly fun! I enrolled shortly thereafter. The next 12 weeks was the best - and most grueling - time of my life. Since completing the course, I've successfully switched careers, working as a Software Engineer at a VR startup.</p>
             </div>
 
         </div>
