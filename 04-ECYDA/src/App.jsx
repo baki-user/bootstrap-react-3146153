09f2b4be1d47a2cf3f4ef451a0e3647fc30c1c50
Nicolás <nicolmaderamc123@gmail.com>
2025-11-25 13:24:49 -0500

    actualizacion

---
+++ 04-ECYDA/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css"
+import { Header } from "./components/Header.jsx"
+
+export const App = () => {
+    return(
+        <>
+        <Header
+         headerColor="bg-green"
+         header2Color="bg.lightColor"
+        />
+        </>
+    )
+}
